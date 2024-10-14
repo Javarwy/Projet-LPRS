e27979463f295889ef0e19cc26bb45fa952f0a8d
Raphael Cazin <44144450+Javarwy@users.noreply.github.com>
2024-10-14 14:02:37 +0200

    Création d'un dossier controlleur

--- PHP/model/Entreprises.php
+++ PHP/model/Entreprises.php
@@ -4,5 +4,20 @@
     private $idEntreprises;
     private $nom;
 
+    public function __construct(array $donnee) {
+      $this->hydrate($donnee);
+  }
+
+    public function hydrate(array $donnee){
+      foreach($donnee as $key => $value){
+          $method = 'set'.ucfirst($key);
+          if(method_exists($this,$method)){
+              $this->$method($value);
+          }
+      }
+  }
+
+
+
    }
 ?>
