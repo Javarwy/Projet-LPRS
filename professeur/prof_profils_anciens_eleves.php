68c4f01e52b9d65fe15577a913bcc35eec97499f
Yohan VIGNEZ <VIGNEZ_Yo@lprs.local>
2024-10-14 15:46:12 +0200

    Création de la page Profils des anciens élèves pour la section professeurs avec affichage des alumni sur tableau

---
+++ professeur/prof_profils_anciens_eleves.php
@@ -0,0 +1,353 @@
+<?php
+include '../PHP/bdd/Bdd.php';
+$bdd = new Bdd;
+$req = $bdd->getBdd()->query('SELECT a.REF_UTILISATEUR as id, u.nom, u.prenom, u.email, e.nom as entreprise FROM utilisateur as u INNER JOIN alumni as a ON u.id_utilisateur = a.REF_UTILISATEUR INNER JOIN entreprise as e ON e.id_entreprise = a.REF_ENTREPRISE;');
+$res = $req->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="../images/fevicon/new_favicon.jpg" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>LPRS - Lycée Privé Robert Schuman</title>
+
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!-- font awesome style -->
+  <link href="../css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="../css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="../css/responsive.css" rel="stylesheet" />
+
+  <style>
+      .dropbtn {
+          font-size: 16px;
+          border: none;
+          outline: none;
+          color: white;
+          padding: 14px 16px;
+          background-color: inherit;
+          font-family: inherit;
+          margin: 0;
+      }
+      .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: #f9f9f9;
+          min-width: 160px;
+          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+          z-index: 1;
+      }
+
+      .dropdown-content a {
+          float: none;
+          color: black;
+          padding: 12px 16px;
+          text-decoration: none;
+          display: block;
+          text-align: left;
+      }
+
+      .dropdown-content a:hover {
+          background-color: #ddd;
+      }
+
+      .dropdown:hover .dropdown-content {
+          display: block;
+      }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="../index.php">
+            <span>
+              <img src="../images/RobertSchuman.webp" alt="" width="150" height="100" />
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  ">
+              <li class="nav-item">
+                <a class="nav-link" href="../index.php">Accueil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="etudiant.php">Etudiant</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="alumni.php">Alumni</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="entreprises.php">Entreprises</a>
+              </li>
+              <li class="nav-item">
+                  <div class="dropdown">
+                      <button class="nav-link dropbtn">Professeur
+                      </button>
+                      <div class="dropdown-content">
+                          <a href="#">Profils des anciens élèves</a>
+                          <a href="#">Profils des étudiants actuels</a>
+                          <a href="#">Publication d'événements</a>
+                          <a href="#">Section d'offres</a>
+                      </div>
+                  </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../contact.php">Contact</a>
+              </li>
+            </ul>
+            <div class="user_optio_box">
+              <a href="">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+  <!-- service section -->
+
+  <section class="service_section">
+    <div class="container-fluid">
+      <div class="row">
+          <div style="text-align: center; margin: auto;">
+              <br>
+              <h2 style="color: #19c880">Profils des anciens élèves</h2>
+              <br>
+              <h5>Annuaire des alumni</h5>
+              <table border="1px" style="text-align: center; margin:auto;">
+                  <tr>
+                      <th hidden="hidden">Id</th>
+                      <th>Nom</th>
+                      <th>Prénom</th>
+                      <th>Email</th>
+                      <th>Entreprise</th>
+                  </tr>
+                  <?php
+                  if (empty($res)) {
+                  ?>
+                      <tr>
+                          <td colspan="5">Aucun alumni trouvé.</td>
+                      </tr>
+                  <?php
+                  } else {
+                      foreach($res as $alumni){
+                  ?>
+                    <tr>
+                        <td hidden="hidden"><?php echo $alumni['id'] ?></td>
+                        <td><?php echo $alumni['nom'] ?></td>
+                        <td><?php echo $alumni['prenom'] ?></td>
+                        <td><?php echo $alumni['email'] ?></td>
+                        <td><?php echo $alumni['entreprise'] ?></td>
+                    </tr>
+                  <?php
+                      }
+                  }
+                  ?>
+              </table>
+              <br>
+          </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end service section -->
+
+  <!-- info section -->
+  <section class="info_section layout_padding2">
+    <div class="container">
+      <div class="info_logo">
+        <h2>
+          HandTime
+        </h2>
+      </div>
+      <div class="row">
+
+        <div class="col-md-3">
+          <div class="info_contact">
+            <h5>
+              About Shop
+            </h5>
+            <div>
+              <div class="img-box">
+                <img src="../images/location-white.png" width="18px" alt="">
+              </div>
+              <p>
+                Address
+              </p>
+            </div>
+            <div>
+              <div class="img-box">
+                <img src="../images/telephone-white.png" width="12px" alt="">
+              </div>
+              <p>
+                +01 1234567890
+              </p>
+            </div>
+            <div>
+              <div class="img-box">
+                <img src="../images/envelope-white.png" width="18px" alt="">
+              </div>
+              <p>
+                demo@gmail.com
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info_info">
+            <h5>
+              Informations
+            </h5>
+            <p>
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="info_insta">
+            <h5>
+              Instagram
+            </h5>
+            <div class="insta_container">
+              <div class="row m-0">
+                <div class="col-4 px-0">
+                  <a href="">
+                    <div class="insta-box b-1">
+                      <img src="../images/w1.png" alt="">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-4 px-0">
+                  <a href="">
+                    <div class="insta-box b-1">
+                      <img src="../images/w2.png" alt="">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-4 px-0">
+                  <a href="">
+                    <div class="insta-box b-1">
+                      <img src="../images/w3.png" alt="">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-4 px-0">
+                  <a href="">
+                    <div class="insta-box b-1">
+                      <img src="../images/w4.png" alt="">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-4 px-0">
+                  <a href="">
+                    <div class="insta-box b-1">
+                      <img src="../images/w5.png" alt="">
+                    </div>
+                  </a>
+                </div>
+                <div class="col-4 px-0">
+                  <a href="">
+                    <div class="insta-box b-1">
+                      <img src="../images/w6.png" alt="">
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="info_form ">
+            <h5>
+              Newsletter
+            </h5>
+            <form action="">
+              <input type="email" placeholder="Enter your email">
+              <button>
+                Subscribe
+              </button>
+            </form>
+            <div class="social_box">
+              <a href="">
+                <img src="../images/fb.png" alt="">
+              </a>
+              <a href="">
+                <img src="../images/twitter.png" alt="">
+              </a>
+              <a href="">
+                <img src="../images/linkedin.png" alt="">
+              </a>
+              <a href="">
+                <img src="../images/youtube.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info_section -->
+
+  <!-- footer section -->
+  <section class="footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span> All Rights Reserved By
+        <a href="https://html.design/">Free Html Templates</a>
+      </p>
+    </div>
+  </section>
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
+  <!-- popper js -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script type="text/javascript" src="../js/bootstrap.js"></script>
+  <!-- custom js -->
+  <script type="text/javascript" src="../js/custom.js"></script>
+  <!-- Google Map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+  </script>
+  <!-- End Google Map -->
+
+</body>
+
+</html>
