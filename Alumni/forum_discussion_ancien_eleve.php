e0bcf76719aab442dc7fd2e89e44b42ca8499438
Raphaël Munoz <r.munoz@lprs.fr>
2024-10-28 17:18:03 +0100

    commit du forum de discussion pour alumni

---
+++ Alumni/forum_discussion_ancien_eleve.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forum de Discussion</title>
+    <meta charset="UTF-8">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            width: 80%;
+            margin: 40px auto;
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ddd;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .topic {
+            margin-bottom: 20px;
+        }
+
+        .topic h2 {
+            margin-top: 0;
+        }
+
+        .post {
+            margin-bottom: 20px;
+            padding: 20px;
+            border-bottom: 1px solid #ccc;
+        }
+
+        .post:last-child {
+            border-bottom: none;
+        }
+
+        .post .author {
+            font-weight: bold;
+            color: #337ab7;
+        }
+
+        .post .date {
+            color: #666;
+            font-size: 0.8em;
+        }
+
+        .reply {
+            margin-left: 40px;
+        }
+
+        .reply .author {
+            color: #337ab7;
+        }
+
+        .reply .date {
+            color: #666;
+            font-size: 0.8em;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Forum de Discussion</h1>
+    <p>Bienvenue sur notre forum de discussion !</p>
+
+    <div class="topic">
+        <h2>Industrie</h2>
+        <p>Description du sujet</p>
+        <div class="post">
+            <p>Lorem Ipsum</p>
+            <p class="author">Lorem Ipsum</p>
+            <p class="date">Lorem Ipsum</p>
+        </div>
+        <div class="post">
+            <p>Lorem Ipsum</p>
+            <p class="author">Lorem Ipsum</p>
+            <p class="date">Lorem Ipsum</p>
+            <div class="reply">
+                <p>Lorum Ipsum</p>
+                <p class="author">Lorem Ipsum</p>
+                <p class="date">Lorem Ipsum</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="topic">
+        <h2>Mentorat</h2>
+        <p>Description du sujet</p>
+        <div class="post">
+            <p>Lorem Ipsum</p>
+            <p class="author">Lorem Ipsum</p>
+            <p class="date">Lorem Ipsum</p>
+        </div>
+        <div class="post">
+            <p>Lorem Ipsum</p>
+            <p class="author">Lorem Ipsum</p>
+            <p class="date">Lorem Ipsum</p>
+        </div>
+    </div>
+
+    <div class="topic">
+        <h2>Entrepreneuriat</h2>
+        <p>Description du sujet</p>
+        <div class="post">
+            <p>Lorem Ipsum</p>
+            <p class="author">Lorem Ipsum</p>
+            <p class="date">Lorem Ipsum</p>
+        </div>
+        <div class="post">
+            <p>Lorem Ipsum</p>
+            <p class="author">Lorem Ipsum</p>
+            <p class="date">Lorem Ipsum</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
+
+<div class="topic">
+    <h2>Mentorat</h2>
+    <p>Description du sujet</p>
+    <div class="post">
+        <p>Lorem Ipsum</p>
+        <p class="author">Lorem Ipsum</p>
+        <p class="date">Lorem Ipsum</p>
+    </div>
+    <div class="post">
+        <p>Lorem Ipsum</p>
+        <p class="author">Lorem Ipsum
+    </div>
+</div>
+
+<div class="topic">
+    <h2>Entrepreneuriat</h2>
+    <p>Description du sujet</p>
+    <div class="post">
+        <p>Lorem Ipsum</p>
+        <p class="author">Lorem Ipsum</p>
+        <p class="date">Lorem Ipsum</p>
+    </div>
+    <div class="post">
+        <p>Message 6</p>
+        <p class="author">Lorem Ipsum</p>
+        <p class="date">Lorem Ipsum</p>
+    </div>
+</div>
+</div>
+</body>
+</html>
+
+
