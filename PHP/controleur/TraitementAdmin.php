f58cdf4e226dcc81ab4997b6f4194c82982afde3
Raphael Cazin <44144450+Javarwy@users.noreply.github.com>
2024-12-09 16:31:05 +0100

    [Tentative de FIX] TraitementAdmin.php

--- PHP/controleur/TraitementAdmin.php
+++ PHP/controleur/TraitementAdmin.php
@@ -11,30 +11,36 @@ if (isset($_POST['DeleteUtilisateur']) && isset($_POST['id_utilisateur'])) {
     $del_uti = $bdd->getBdd()->prepare('DELETE FROM utilisateur WHERE id_utilisateur = ["id_utilisateur"]');
     $del_uti->execute();
     echo "Requête exécutée avec succès !";
+    header("Location: ../../admin_dashboard.php");
 }
 
-if (isset($_POST['DeleteEvenement']) && isset($_POST['id_evenement'])) {
+if (isset($_POST['DeleteEvenement'])) {
     $del_ev = $bdd->getBdd()->prepare('DELETE FROM evenement WHERE id_evenement = ["id_evenement"]');
     $del_ev->execute();
     echo "Requête exécutée avec succès !";
+    header("Location: ../../admin_dashboard.php");
 }
 
-if (isset($_POST['DeleteOffre']) && isset($_POST['id_offre'])) {
+if (isset($_POST['DeleteOffre'])) {
     $del_of = $bdd->getBdd()->prepare('DELETE FROM offre WHERE id_offre = ["id_offre"]');
     $del_of->execute();
     echo "Requête exécutée avec succès !";
+    header("Location: ../../admin_dashboard.php");
 }
 
-if (isset($_POST['ValiderUtilisateur']) && isset($_POST['id_utilisateur'])) {
+if (isset($_POST['ValiderUtilisateur'])) {
     $val_uti = $bdd->getBdd()->prepare('UPDATE utilisateur SET active = 1 WHERE id_utilisateur = ["id_utilisateur"]');
     $var_uti->execute();
     echo "Requête exécutée avec succès !";
+    header("Location: ../../admin_dashboard.php");
+
 }
 
-if (isset($_POST['ValiderEvenement']) && isset($_POST['id_evenement'])) {
+if (isset($_POST['ValiderEvenement'])) {
     $val_ev = $bdd->getBdd()->prepare('UPDATE evenement SET verification = 1 WHERE id_offre = ["id_offre"]');
     $val_ev->execute();
     echo "Requête exécutée avec succès !";
+    header("Location: ../../admin_dashboard.php");
 }
 
 ?>
